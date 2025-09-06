6880ab62b092c74eee6cde67c669e137001bfd2b
yyypluto <mfarrasashshiddiqi@gmail.com>
2025-09-06 11:48:53 +0700

    fix: auction order string

--- pkg/auction/repository.go
+++ pkg/auction/repository.go
@@ -45,7 +45,7 @@ func (r *repository) List(ctx context.Context, offset, limit int, startDate, end
 	}
 
 	var collection []entities.Auction
-	if err := query.Offset(offset).Limit(limit).Order(orderStr).Find(&collection).Error; err != nil {
+	if err := query.Offset(offset).Limit(limit).Order(*orderStr).Find(&collection).Error; err != nil {
 		return nil, 0, err
 	}
 
